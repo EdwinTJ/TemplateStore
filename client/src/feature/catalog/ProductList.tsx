c1c34d2ea0a80ceec4622a350369bffe4b5d4327
EdwinTJ <edwin16x@gmail.com>
2022-11-01 13:52:31 -0600

    Pagination

--- client/src/feature/catalog/ProductList.tsx
+++ client/src/feature/catalog/ProductList.tsx
@@ -1,20 +1,26 @@
 import { Grid } from "@mui/material";
 import { Product } from "../../App/models/product";
+import { useAppSelector } from "../../App/store/configureStore";
 import ProductCard from "./ProductCard";
+import ProductCardSkeleton from "./ProductCardSkeleton";
 
 interface Props {
   products: Product[];
 }
-export default function PorductList({ products }: Props) {
+
+export default function ProductList({ products }: Props) {
+  const { productsLoaded } = useAppSelector((state) => state.catalog);
   return (
-    <>
-      <Grid container spacing={4}>
-        {products.map((product) => (
-          <Grid item xs={4}>
-            <ProductCard key={product.id} product={product} />
-          </Grid>
-        ))}
-      </Grid>
-    </>
+    <Grid container spacing={4}>
+      {products.map((product) => (
+        <Grid item xs={4} key={product.id}>
+          {!productsLoaded ? (
+            <ProductCardSkeleton />
+          ) : (
+            <ProductCard product={product} />
+          )}
+        </Grid>
+      ))}
+    </Grid>
   );
 }
